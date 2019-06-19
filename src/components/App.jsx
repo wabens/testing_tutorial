075d19223355bd1fbfcf8f0cf2422adeff01e0ed
wabens <waltkbenson@gmail.com>
2019-06-19 14:50:35 -0500

    error due to vesion diffs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,11 +17,6 @@ export default class App extends Component {
           <nav className="blue lighten-2">
             <div className="nav-wrapper">
               <a href="#" className="brand-logo center">Courses</a>
-              {/* <ul id="nav-mobile" className="left hide-on-med-and-down">
-                <li><a href="#">React</a></li>
-                <li><a href="#">React Native</a></li>
-                <li><a href="#">Framer</a></li>
-              </ul> */}
             </div>
           </nav>
         </div>
--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,21 +1,39 @@
 import data from '../../data/courses.json';
 
+// Numbers
 const numItems = data.length;
 
 test('Number of items = 12', () => {
-    expect(numItems).toBe(12);
+  expect(numItems).toBe(12);
 });
 
 test('Number of items to be greader than 12', () => {
-    expect(numItems).toBeGreaterThanOrEqual(12);
+  expect(numItems).toBeGreaterThanOrEqual(12);
 });
 
+// STrings
 const dataTest = data[0].title;
 
 test('Contains JS', () => {
-    expect(dataTest).toMatch(/JS/); // some regex
+  expect(dataTest).toMatch(/JS/); // some regex
 });
 
 test('Contains React', () => {
-    expect(dataTest).toContain('React');
+  expect(dataTest).toContain('React');
+});
+
+// Arrays
+const data2 = ['React Native', 'MeteorJS'];
+
+test('The list of courses mentions React Native and MeteorJS', () => {
+  expect(['React Native', 'MeteorJS', 'React']).toEqual(expect.arrayContaining(data2)); 
+});
+
+// Objects
+test('Expect first course to have property of title', () => {
+  expect(data[0]).toHaveProperty('title');
+});
+
+test('Expect first course to have 31,266 views', () => {
+  expect(data[0]).toHaveProperty('views', '31,266');
 });
